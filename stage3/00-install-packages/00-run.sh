14ec55f76f2c40de86ba7aa55ad8cde08950ca85
Matt Robenolt <matt@ydekproductions.com>
2018-08-05 15:20:54 -0700

    Lay down working directory for service

--- stage3/00-install-packages/00-run.sh
+++ stage3/00-install-packages/00-run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
+install -d								${ROOTFS_DIR}/var/lib/strut/
 install -m 644 files/strut.service		${ROOTFS_DIR}/etc/systemd/system/
 install -m 744 files/strut				${ROOTFS_DIR}/usr/bin/
 
 on_chroot << EOF
 systemctl enable strut
-systemctl enable ssh
 EOF
